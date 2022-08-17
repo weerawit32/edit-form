14bbbb5d06e6a6f1cc910f5900d0c83a45474dfe
weerawit32 <vasthabo@hotmail.com>
2022-08-18 01:53:36 +0700

    test-form

--- edit-form/src/App.js
+++ edit-form/src/App.js
@@ -1,12 +1,46 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [person, setPerson] = useState(() => {
+    return {
+      id: "",
+      name: "",
+      age: "",
+    };
+  });
+
+  const { id, name, age } = person;
+
+  const handleOnChange = (e) => {
+    setPerson({ ...person, [e.target.name]: e.target.value });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(person);
+  };
+
   return (
     <div className="App">
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
+      <form onSubmit={handleOnSubmit}>
+        <input
+          placeholder="Enter Your name..."
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleOnChange}
+        ></input>
+        <input
+          placeholder="Enter Your age..."
+          type="text"
+          name="age"
+          value={age}
+          onChange={handleOnChange}
+        ></input>
+        <button>OK</button>
+      </form>
     </div>
   );
 }
